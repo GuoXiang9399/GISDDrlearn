516cd4d576e86b8e6c4c7f40bc3adf4fd11cce7b
Xiang Guo <guoxiang199399@163.com>
2024-08-20 22:34:55 +0800

    update scripts

--- ui.R
+++ ui.R
@@ -6,68 +6,71 @@
   library(readxl)
   library(ape)
   library(caret)
+  library(shinythemes)
 ###############################################################################
 # Shiny UI
   fluidPage(
-    theme = "bootstrap.css",
-    includeCSS("www/styles.css"),
+    #theme = "bootstrap.css",
+    #includeCSS("www/styles.css"),
     #######################################################################
     navbarPage(
-      " GISDDrlearn ",
+      HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#"> GISDDrlearn </a>'), id="nav",
+      windowTitle = "GISDDrlearn",
+      theme = shinytheme("flatly"), collapsible = TRUE,
      ######################################################################
       tabPanel(
-        "About/Usage 简介",
-        h2("GISDDrlearn"),
-        h5("Our study has established a reproduceable and comparable global genotyping framework of DENV with contextualizing spatiotemporal epidemiological information before. The defned framework was discriminated with three hierarchical layers of genotype, subgenotype and clade with respective mean pairwise distances 2–6%, 0.8–2%, and ≤0.8%. This framework reveals that the persisting traditional endemic sourcing, the emerging epidemic difusing, and the probably hidden epidemics are the crucial drivers of the rapid global spread of dengue. "),
-        h5("GISDDrlearn is a easy-used R Shiny app for assigning DENV sequences to serotype, genotype, subgenotype, and clade based on Random Forest method. As one of the import machine learning model, random forest is an ensemble of secision trees, each built using a subset of the training sample. The structural from show a interesting connection between Random Forest and evolution tree."),
-        h5("Due to the accessibility and afordability of E gene, our highresolution genotyping can be employed as a transitional or linkage scheme reconciling the classical E gene genotyping with the emerging genomic epidemiology. For all GISDDrlearn models, the model was trained using DENV complete E sequences and their designated serotypes, genotypes, subgenotyps, and clades. GISDDrlearn can assign 1000 E sequences in ~30 seconds."),
+        "Introduction & Usage",
+        h4("Introduction"),
+        h5("The rapid expansion in virus genome data production has necessitated development of novel analytical methods capable of handling more genomes than previously accessible."),
+        h5("As an important machine learning model, Random Forest is an ensemble of decision trees, each constructed using a subset of the training sample. The structural analogy reveals an intriguing connection between Random Forest and evolution trees."),
+        h5("With this concept, we developed GISDDrlearn, an user-friendly R Shiny application, for assigning DENV sequences to serotype, genotype, subgenotype, and clade based on the Random Forest."),
+        h5("Due to accessibility and affordability of the E gene of DENV, our high-resolution genotyping with GISDDrlearn can bridge classical E-gene-based genotyping methods with emerging genomic epidemiology."),
+        h5("All GISDDrlearn models trained with complete E sequences of DENV and their designated serotypes, genotypes, subgenotyps, and clades can assign 1000 E sequences in approximately 30 seconds."),
         hr(),
-        h2("Usage"),
-        h5("(1) Import fasta files (Complete E gene 1485bp)"),
-        h5("(2) Genotyping based on Random Forest"),
-        h5("(3) Result check"),
-        hr(),
-        h2("Data source"),
-        a(href = "http://www.bic.ac.cn/GISDD/#/",
-          "GISDD (http://www.bic.ac.cn/GISDD/#/)",
+        h4("Demo file"),
+        a(href = "https://www.smu.edu.cn/rdyjs/index.htm",
+          "file download",
           target = "_blank"),
         hr(),
-        h2("Contact"),
-        a(href = "https://www.smu.edu.cn/rdyjs/info/1003/1355.htm",
-          "Xiang Guo",
-          target = "_blank"),
-        h5("Institutes of Tropical Medicine, Southern Medical University, China"),
-        hr(),
-        h2("References"),
-        h5("1. DENV genotyping framework: Infectious Diseases of Poverty (2022) 11:107"),
-        a(href = "https://doi.org/10.1186/s40249-022-01024-5",
-          "A unifed global genotyping framework of dengue virus serotype-1 for a stratifed coordinated surveillance strategy of dengue epidemics",
-          target = "_blank"),
-        h5("2. Epidemiology study:"),
-        a(href = "https://doi.org/10.21203/rs.3.rs-3902313/v1",
-          "Phylodynamics unveils invading and diffusing patterns of dengue virus serotype-1 in Guangdong China from 1990 to 2019 under a global genotyping framework",
-          target = "_blank"),
+        h4("Usage"),
+        h5("(1). Click the Genotyping TabPanel"),
+        h5("(2). Check the GISDD version"),
+        h5("(3). Choose subgenotype & clade model"),
+        h5("(4). Import fasta files (NOTE: must Complete E gene)"),
+        h5("(5). Click the Submit bottom"),
+        h5("(6). Check the genotyping results"),
+        img(src = "Usage.png", alt = "D1 Genome Image", style = "width:75%;height:auto;"),
+        hr()
       ),
      ######################################################################
-     tabPanel(
-       "GISDD frame 分型框架",
-       sidebarLayout(
-         sidebarPanel(
-           radioButtons("picture", "Picture:",
-                        c("structure", "frame"))
-         ),
-         mainPanel(
-           imageOutput("image2")
-         )
-       )
-     ),
+      tabPanel(
+        "Model",
+        h4("Training process"),
+        h5("Developed to enhance the classification of DENV genotypes, the GISDDrlearn employed a random forest classification algorithm to predict serotype, genotype, subgenotype, and clade of DENV genotypes."),
+        h5("The model was crafted using the R language with the ‘caret’ (version 6.094) and ‘randomForest’ (version 4.71.1) r-package, employing the optimal model (parameters of mtry and splitrule) were selected based on the value of accuracy."),
+        h5("The model’s performance was validated through receiver operating characteristic (ROC) analysis with ten-fold cross-validation, calculating the area under the curve (AUC) for each sequence's serotype, genotype, subgenotype, and clade predictions in the training set."),
+        h5("The trained GISDDrlearn model along with header files, metadata and detailed notes on genotype, subgenotype, and clade, are accessible on GitHub repository."),
+        a(href = "https://github.com/GuoXiang9399/GISDDrlearn-training",
+          "GISDDrlearn-training (https://github.com/GuoXiang9399/GISDDrlearn-training)",
+          target = "_blank"),
+        hr(),
+        h4("Model"),
+        h5("Current models were trained separately on subgenotypes and clades across various serotypes on June 2020."),
+        a(href = "https://github.com/GuoXiang9399/GISDDrlearn-training/tree/main/Result",
+          "Model list:",
+          target = "_blank"),
+        h5("(1) rffit_GISDD.1.3.2_D1_Subgenotype & rffit_GISDD.1.3.2_D1_Clade"),
+        h5("(2) rffit_GISDD.1.3.2_D2_Subgenotype & rffit_GISDD.1.3.2_D2_Clade"),
+        h5("(3) rffit_GISDD.1.3.2_D3_Subgenotype & rffit_GISDD.1.3.2_D3_Clade"),
+        h5("(4) rffit_GISDD.1.3.2_D4_Subgenotype & rffit_GISDD.1.3.2_D4_Clade")
+      ),
      ######################################################################
       tabPanel(
-        "Genotyping 分型工具",
+        "Genotyping",
          sidebarLayout(
           sidebarPanel(
             h4("Current GISDD version:"),
-            h1("v1.3.2"),
+            h3("v1.3.2"),
             tags$hr(),
             selectInput("dataset1", "Choose a subgenotype model:",
                         choices = c("rffit_GISDD.1.3.2_D1_Subgenotype", 
@@ -85,8 +88,6 @@
                       accept = c("text/csv",".csv",".fas",".fasta",".fa",
                                  "text/comma-separated-values,text/plain")),
             tags$hr(),
-            checkboxInput("header", "Header", TRUE),
-            tags$hr(),
             radioButtons("disp", "Display",
                          choices = c(Head = "head",
                                      All = "all"),
@@ -100,6 +101,31 @@
           )
         )
       ),
+     ######################################################################
+     tabPanel(
+       "About",
+       h4("Contact"),
+       a(href = "https://www.smu.edu.cn/rdyjs/info/1003/1355.htm",
+         "Xiang Guo  Institutes of Tropical Medicine, Southern Medical University, China",
+         target = "_blank"),
+       hr(),
+       h4("Sources"),
+       a(href = "http://www.bic.ac.cn/GISDD/#/",
+         "GISDD (http://www.bic.ac.cn/GISDD/#/)",
+         target = "_blank"),
+       hr(),
+       h4("References"),
+       h5("(1). DENV genotyping framework"),
+       h5("Infectious Diseases of Poverty (2022) 11:107"),
+       a(href = "https://doi.org/10.1186/s40249-022-01024-5",
+         "A unifed global genotyping framework of dengue virus serotype-1 for a stratifed coordinated surveillance strategy of dengue epidemics",
+         target = "_blank"),
+       h5("(2). Molecular epidemiology in China"),
+       h5("Infectious Diseases of Poverty (2024)"),
+       a(href = "https://doi.org/10.1186/s40249-024-01211-6",
+         "Phylodynamics unveils invading and diffusing patterns of dengue virus serotype-1 in Guangdong China from 1990 to 2019 under a global genotyping framework",
+         target = "_blank")
+     )
      #######################################################################
     )
   )
